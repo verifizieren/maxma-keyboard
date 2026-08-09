4d75a1b683805020817f4090c369b8acce2a5f24
Max Mendes <135263966+verifizieren@users.noreply.github.com>
2026-08-09 16:50:52 +0200

    Add base and FN layers, encoder map, and spare VIA layers

--- keyboards/gmmk/pro/rev1/ansi/keymaps/maxma/keymap.c
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/maxma/keymap.c
@@ -1,68 +1,104 @@
-/* Copyright 2021 Glorious, LLC <salman@pcgamingrace.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// Copyright 2026 Max Mendes (@verifizieren)
+// SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
+
+enum layers {
+    _BASE,
+    _FN,
+    _L2,  // spare, for VIA
+    _L3,  // spare, for VIA
+};
+
+enum custom_keycodes {
+    RESET_CFG = QK_KB_0,  // tap: reset lighting. hold 2s: wipe EEPROM.
+};
+
+// No HID keycode mutes a microphone; the host has to. This sends a combo
+// unlikely to collide, to be bound as the mute hotkey in Discord/OBS/etc.
+#define MIC_MUTE C(A(S(KC_M)))
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-//      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12	     Prt           Rotary(Mute)
-//      ~        1        2        3        4        5        6        7        8        9        0         -       (=)	     BackSpc           Del
-//      Tab      Q        W        E        R        T        Y        U        I        O        P        [        ]        \                 PgUp
-//      Caps     A        S        D        F        G        H        J        K        L        ;        "                 Enter             PgDn
-//      Sh_L              Z        X        C        V        B        N        M        ,        .        ?                 Sh_R     Up       End
-//      Ct_L     Win_L    Alt_L                               SPACE                               Alt_R    FN       Ct_R     Left     Down     Right
-
-
-    // The FN key by default maps to a momentary toggle to layer 1 to provide access to the QK_BOOT key (to put the board into bootloader mode). Without
-    // this mapping, you have to open the case to hit the button on the bottom of the PCB (near the USB cable attachment) while plugging in the USB
-    // cable to get the board into bootloader mode - definitely not fun when you're working on your QMK builds. Remove this and put it back to KC_RGUI
-    // if that's your preference.
-    //
-    // To put the keyboard in bootloader mode, use FN+backslash. If you accidentally put it into bootloader, you can just unplug the USB cable and
-    // it'll be back to normal when you plug it back in.
-    //
-    // This keyboard defaults to 6KRO instead of NKRO for compatibility reasons (some KVMs and BIOSes are incompatible with NKRO).
-    // Since this is, among other things, a "gaming" keyboard, a key combination to enable NKRO on the fly is provided for convenience.
-    // Press Fn+N to toggle between 6KRO and NKRO. This setting is persisted to the EEPROM and thus persists between restarts.
-    [0] = LAYOUT(
+    /* Base — stock ANSI, except the right column reads Del / Ins / Home / End.
+     *
+     * ,---------------------------------------------------------------.  ,------.
+     * | Esc| F1| F2| F3| F4| F5| F6| F7| F8| F9|F10|F11|F12|   PrtSc   |  | Knob |
+     * |---------------------------------------------------------------|  |------|
+     * |  ` | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | - | = |   Bksp    |  | Del  |
+     * |---------------------------------------------------------------|  |------|
+     * | Tab | Q | W | E | R | T | Y | U | I | O | P | [ | ] |    \     |  | Ins  |
+     * |---------------------------------------------------------------|  |------|
+     * | Caps | A | S | D | F | G | H | J | K | L | ; | ' |   Enter     |  | Home |
+     * |---------------------------------------------------------------|  |------|
+     * | Shift  | Z | X | C | V | B | N | M | , | . | / | Shift  |  Up  |  | End  |
+     * |---------------------------------------------------------------|  |------|
+     * | Ctl | Win | Alt |        Space        | Alt | Fn | Ctl | L D R |
+     * `---------------------------------------------------------------'
+     */
+    [_BASE] = LAYOUT(
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DEL,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_PGUP,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_PGDN,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          KC_INS,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_HOME,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(_FN), KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
-    [1] = LAYOUT(
-        _______, KC_MYCM, KC_WHOM, KC_CALC, KC_MSEL, KC_MPRV, KC_MNXT, KC_MPLY, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______,          _______,
-        _______, RM_TOGG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
-        _______, _______, RM_VALU, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,          _______,
-        _______, _______, RM_VALD, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______,
-        _______,          _______, RM_HUEU, _______, _______, _______, NK_TOGG, _______, _______, _______, _______,          _______, RM_NEXT, _______,
-        _______, _______, _______,                            _______,                            _______, _______, _______, RM_SPDD, RM_PREV, RM_SPDU
+    /* FN — a short list of assignments, everything else transparent.
+     * (Task 6 adds SOCDTOG on S; no count is written here so this comment
+     * cannot go stale.)
+     *
+     * Esc      reset (tap: lighting, hold 2s: full wipe)
+     * F1-F4    prev / play-pause / next / stop
+     * F5-F7    mute / vol- / vol+
+     * F8       mic mute (Ctrl+Alt+Shift+M)
+     * F9,F10   Scroll Lock, Pause
+     * PrtSc    bootloader
+     * -,=      RGB brightness down/up
+     * Bksp     Delete
+     * K        NKRO toggle          N       Num Lock
+     * arrows   PgUp/PgDn/Home/End   Ins,Home column   PgUp/PgDn
+     * knob     RGB on/off
+     */
+    [_FN] = LAYOUT(
+        RESET_CFG, KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, KC_MUTE, KC_VOLD, KC_VOLU, MIC_MUTE, KC_SCRL, KC_PAUS, _______, _______, QK_BOOT,        RM_TOGG,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RM_VALD, RM_VALU, KC_DEL,           _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_PGUP,
+        _______, _______, _______, _______, _______, _______, _______, _______, NK_TOGG, _______, _______, _______,          _______,          KC_PGDN,
+        _______,          _______, _______, _______, _______, _______, KC_NUM,  _______, _______, _______, _______,          _______, KC_PGUP, _______,
+        _______, _______, _______,                            _______,                            _______, _______, _______, KC_HOME, KC_PGDN, KC_END
     ),
 
+    /* Spare layers. Transparent, but they MUST exist: VIA seeds its EEPROM
+     * copy by reading keymaps[layer] for every dynamic layer, so a short
+     * array gets read past its end and the spares fill with garbage. */
+    [_L2] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______
+    ),
 
+    [_L3] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______,
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______
+    ),
 };
-// clang-format on
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) }
+    [_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [_FN]   = { ENCODER_CCW_CW(RM_VALD, RM_VALU) },
+    [_L2]   = { ENCODER_CCW_CW(_______, _______) },
+    [_L3]   = { ENCODER_CCW_CW(_______, _______) },
 };
 #endif
+// clang-format on
